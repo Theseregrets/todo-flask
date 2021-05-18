9299d5021835cc5f60cb03fcfbc1737a9847eedf
Theseregrets <rishitashaw49@gmail.com>
2021-05-18 09:48:18 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -31,6 +31,33 @@ def hello_world():
     allTodo = Todo.query.all()
     return render_template('index.html', allTodo=allTodo)
 # return 'Hello, World!'
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+
+    if request.method == 'POST':
+        title = request.form["title"]
+        desc = request.form["desc"]
+
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+
+    todo = Todo.query.filter_by(sno=sno).first()
+
+    return render_template('update.html', todo=todo)
 
 
 if __name__ == '__main__':
